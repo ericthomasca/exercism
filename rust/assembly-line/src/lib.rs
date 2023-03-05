8f39f23eb0bfa79c5518f47f8660bb84736e58e9
Eric Thomas <eric@ericthomas.ca>
2023-03-04 20:49:08 -0330

    update .gitignore

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {speed}")
+    if (0..=4).contains(&speed) {
+        speed as f64 * 221.0
+    } else if (5..=8).contains(&speed) {
+        speed as f64 * 221.0 * 0.9
+    } else if (9..=10).contains(&speed) {
+        speed as f64 * 221.0 * 0.77
+    } else {
+        0.0
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {speed}")
+    let cars_per_hour = production_rate_per_hour(speed);
+    (cars_per_hour / 60.0) as u32
 }
