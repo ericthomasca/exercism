d7a12fc8d84a1ad7b1413307ddca0085b7bf021a
Eric Thomas <eric@ericthomas.ca>
2023-03-11 23:09:03 -0330

    java lasagna progress

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,14 @@
+public class Lasagna {
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven() {
+        
+    }
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    // TODO: define the 'totalTimeInMinutes()' method
+}
