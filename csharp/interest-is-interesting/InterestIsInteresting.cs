6a6c2b55b1cb043585463812f85bbb5fbca32704
Eric Thomas <eric@ericthomas.ca>
2023-03-28 18:36:20 -0230

    csharp interest is interesting

---
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -0,0 +1,40 @@
+using System;
+
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance)
+    {
+        return balance switch
+        {
+            < 0m => 3.213f,
+            >= 0m and < 1000m => 0.5f,
+            >= 1000m and < 5000m => 1.621f,
+            >= 5000m => 2.475f,
+        };
+    }
+
+    public static decimal Interest(decimal balance)
+    {
+        var rate = (decimal)InterestRate(balance) / 100m;
+        return balance * rate;
+    }
+
+    public static decimal AnnualBalanceUpdate(decimal balance)
+    {
+        var interest = Interest(balance);
+        return balance + interest;
+    }
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        int years = 0;
+
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            years ++;
+        }
+
+        return years;
+    }
+}
