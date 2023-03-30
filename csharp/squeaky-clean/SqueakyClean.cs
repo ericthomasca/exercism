b56dd5ab5f6c5f4b3270b80a13f8386c15f96800
Eric Thomas <eric@ericthomas.ca>
2023-03-29 22:45:47 -0230

    csharp squeaky clean complete

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,20 +1,24 @@
-using System;
-using System.Text;
+using System.Text.RegularExpressions;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
         // Replace any spaces encountered with underscores
-        var cleanedString = identifier.Replace(' ', '_');
+        var spacelessString = identifier.Replace(' ', '_');
 
         // Replace control characters with the upper case string "CTRL"
-        cleanedString = cleanedString.Replace("\0", "CTRL");
+        var ctrlString = spacelessString.Replace("\0", "CTRL");
 
         // Convert kebab-case to camelCase
-        // Omit characters that are not letters
-        // Omit Greek lower case letters
+        var camelCaseString = Regex.Replace(ctrlString, "-.", x => x.Value.ToUpper().Substring(1));
 
-        return cleanedString;
+        // Omit characters that are not letters
+        var letterOnlyString = Regex.Replace(camelCaseString, @"\W|[0-9]", "");
+        
+        // Omit Greek lower case letters
+        var cleanString = Regex.Replace(letterOnlyString, @"[α-ω]", "");
+
+        return cleanString;
     }
 }
