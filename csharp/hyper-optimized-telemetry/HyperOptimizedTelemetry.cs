44d42f57b75a1d8e5a3165b117edd0b107d57ec4
Eric Thomas <eric@ericthomas.ca>
2023-04-01 20:40:49 -0230

    c# hyper optimized telemetry progress 2

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -5,11 +5,31 @@ public static class TelemetryBuffer
 {
     public static byte[] ToBuffer(long reading)
     {
-        var byteArray switch (reading)
+        byte[] byteArray;
+        
+        switch (reading)
         {
-            < 
+            case > int.MaxValue and <= uint.MaxValue:
+                byteArray = BitConverter.GetBytes((uint)reading).Prepend((byte)(2)).ToArray();
+                break;
+            case > ushort.MaxValue and <= int.MaxValue:
+                byteArray = BitConverter.GetBytes((int)reading).Prepend((byte)(256 - 2)).ToArray();
+                break;
+            case >= ushort.MinValue and <= ushort.MaxValue:
+                byteArray = BitConverter.GetBytes((ushort)reading).Prepend((byte)(2)).ToArray();
+                break;
+            case >= short.MinValue and < ushort.MinValue:
+                byteArray = BitConverter.GetBytes((short)reading).Prepend((byte)(256 - 2)).ToArray();
+                break;
+            case >= int.MinValue and < short.MinValue:
+                byteArray = BitConverter.GetBytes((int)reading).Prepend((byte)(256 - 4)).ToArray();
+                break;
             default:
+                byteArray = BitConverter.GetBytes(reading).Prepend((byte)(256 - 2)).ToArray();
+                break;
         }
+
+        return byteArray;
     }
 
     public static long FromBuffer(byte[] buffer)
