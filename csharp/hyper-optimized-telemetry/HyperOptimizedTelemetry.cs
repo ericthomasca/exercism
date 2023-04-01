8222ceaf84f0ae5e88f70c5e1714ca52837f9dcc
Eric Thomas <eric@ericthomas.ca>
2023-04-01 19:22:38 -0230

    c# hyper optimized telemetry progress

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -5,31 +5,11 @@ public static class TelemetryBuffer
 {
     public static byte[] ToBuffer(long reading)
     {
-        // switch (reading)
-        // {
-        //     case >= -2_147_483_648 and <= -32_769:
-        //     case >= 65_536 and <= 2_147_483_647:
-        //         reading = (int)reading;
-        //         break;
-        //     case >= -32_768 and <= -1:
-        //         reading = (short)reading;
-        //         break;
-        //     case >= 0 and <= 65_535:
-        //         reading = (ushort)reading;
-        //         break;
-        //     case >= 2_147_483_648 and <= 4_294_967_295:
-        //         reading = (uint)reading;
-        //         break;
-        //     default:
-        //         break;
-        // }
-
-        byte[] byteArray = BitConverter.GetBytes(reading);
-
-        byte[] prefixByte = BitConverter.GetBytes(256 - byteArray.Length - 1);
-
-        byte[] prefixedByteArray = prefixByte.Concat(byteArray).ToArray();
-        return prefixedByteArray;
+        var byteArray switch (reading)
+        {
+            < 
+            default:
+        }
     }
 
     public static long FromBuffer(byte[] buffer)
