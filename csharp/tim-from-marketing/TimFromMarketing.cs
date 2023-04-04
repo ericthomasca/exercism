145eb2954ef78fd52302c54fc54e62bf6f26c35b
Eric Thomas <eric@ericthomas.ca>
2023-04-03 22:21:09 -0230

    c# tim from marketing

---
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -0,0 +1,16 @@
+using System;
+
+static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {
+        if (id == null)
+        {
+            return $"{name} - {(department?.ToUpper() ?? "OWNER")}";
+        }
+        else
+        {
+            return $"[{id}] - {name} - {(department?.ToUpper() ?? "OWNER")}";
+        }
+    }
+}
