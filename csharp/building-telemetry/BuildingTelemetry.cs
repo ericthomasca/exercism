a4a7c03ce5748b9d2352bdf3a74040c5092b8c99
Eric Thomas <eric@ericthomas.ca>
2023-04-12 21:58:09 -0230

    c# BuildingTelemetry progress

--- csharp/building-telemetry/BuildingTelemetry.cs
+++ csharp/building-telemetry/BuildingTelemetry.cs
@@ -19,7 +19,7 @@ public class RemoteControlCar
 
     public void SetSponsors(params string[] sponsors)
     {
-        this.sponsors = sponsors;
+        this.sponsors = sponsors ?? Array.Empty<string>();
     }
 
     public string DisplaySponsor(int sponsorNum) 
@@ -28,13 +28,24 @@ public class RemoteControlCar
     public bool GetTelemetryData(ref int serialNum,
         out int batteryPercentage, out int distanceDrivenInMeters)
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.GetTelemetryData() method");
+        if (serialNum > this.latestSerialNum)
+        {
+            this.latestSerialNum = serialNum;
+            batteryPercentage = 80;
+            distanceDrivenInMeters = 4;
+            return true;
+        }
+        else
+        {
+            serialNum = this.latestSerialNum;
+            batteryPercentage = -1;
+            distanceDrivenInMeters = -1;
+            return false;
+        }
     }
 
-    public static RemoteControlCar Buy()
-    {
-        return new RemoteControlCar();
-    }
+    public static RemoteControlCar Buy() => new RemoteControlCar();
+
 }
 
 public class TelemetryClient
@@ -48,6 +59,16 @@ public class TelemetryClient
 
     public string GetBatteryUsagePerMeter(int serialNum)
     {
-        throw new NotImplementedException("Please implement the TelemetryClient.GetBatteryUsagePerMeter() method");
+        bool telemetryData = car.GetTelemetryData(ref serialNum, out int batteryPercentage, out int distanceDrivenInMeters);
+        if (telemetryData || (batteryPercentage == 100 && distanceDrivenInMeters == 0))
+        {
+            return "no data";
+        }
+        else
+        {
+            var usagePerMeter = (100 - batteryPercentage) / distanceDrivenInMeters;
+            return $"usage-per-meter={usagePerMeter}";
+        }
     }
+
 }
